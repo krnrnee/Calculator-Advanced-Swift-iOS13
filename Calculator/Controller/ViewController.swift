24bc7d02b97521e91635c48636830134869fc97e
Karen Turner <krt@karenrenee.com>
2022-04-05 14:43:16 -0600

    Complete through lesson 307 - Dealing with the decimal place

--- Calculator/Controller/ViewController.swift
+++ Calculator/Controller/ViewController.swift
@@ -12,20 +12,71 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
     
-    
+    private var isFinishedTypingNumber: Bool = true
+    private var decimalAlreadyPressed: Bool = false
+    private var num1: Double = 0.0
+    private var num2: Double = 0.0
+    private var lastOperation: String = ""
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         //What should happen when a non-number button is pressed
-    
+        //displayLabel.text = "0"
+        isFinishedTypingNumber = true
+        guard let doubleValue = Double(self.displayLabel.text!) else {
+            fatalError("Error converting display label text to double")
+        }
+        
+        switch sender.currentTitle {
+        case "AC":
+            self.displayLabel.text = "0"
+            num1 = 0.0
+            num2 = 0.0
+        case "+/-":
+            num1 = doubleValue
+            num1 = num1 * -1.0
+            self.displayLabel.text = String(num1)
+        case "%":
+            num1 = doubleValue
+            num1 = num1 / 100.0
+            self.displayLabel.text = String(num1)
+        case "+":
+            num1 += doubleValue
+            self.displayLabel.text = String(num1)
+        case "-":
+            num1 -= doubleValue
+            self.displayLabel.text = String(num1)
+        default:
+            self.displayLabel.text = "0"
+        }
+        
     }
-
+    
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
         
         //What should happen when a number is entered into the keypad
-    
+        if let numValue = sender.currentTitle {
+            if isFinishedTypingNumber {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+           } else {
+               if numValue == "." {
+                   guard let currentDisplayValue = Double(displayLabel.text!) else {
+                       fatalError("Error converting display label text to double")
+                   }
+                   
+                   let isInt = floor(currentDisplayValue) == currentDisplayValue
+ 
+                   if !isInt {
+                       return
+                   }
+               }
+                displayLabel.text! += numValue
+            }
+        }
+        
     }
-
+    
 }
 
